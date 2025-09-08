5ce6aaee00e1a02bd082e7e9b332a95eef7039e0
urboy-code <42699288+urboy-code@users.noreply.github.com>
2025-09-08 09:17:58 +0700

    feat(database): Create projects and tasks tables with models and relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function projects(){
+        return $this->hasMany(Project::class);
+    }
 }
